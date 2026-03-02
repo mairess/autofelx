0b4ebe17edf8c3154729c91ca782d754778d7621
mairess <trabalho.maires@gmail.com>
2026-03-01 22:29:21 -0300

    fix: cors

--- backend/src/main/java/com/autoflex/backend/configuration/CorsConfig.java
+++ backend/src/main/java/com/autoflex/backend/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.autoflex.backend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,18 +12,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+  @Value("${app.frontend.url}")
+  private String frontendUrl;
+
   /**
    * Cors configurer web mvc configurer.
    *
    * @return the web mvc configurer
    */
   @Bean
-  public WebMvcConfigurer orsConfigurer() {
+  public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
+            .allowedOrigins("http://localhost:5173", frontendUrl)
             .allowedMethods("*")
             .allowedHeaders("*");
       }
